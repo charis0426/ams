c98ca65abf359842a7bc8bb24f4360f86d1b90fc
xianqinglin <810282319@qq.com>
2018-08-05 14:19:55 +0800

    加入判断post和get方法

--- application/common.php
+++ application/common.php
@@ -1,4 +1,5 @@
 <?php
+use \think\Request;
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
@@ -10,3 +11,5 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+defined('IS_POST') or define('IS_POST', Request::instance()->isPost());
+defined('IS_GET') or define('IS_GET', Request::instance()->isGet());
