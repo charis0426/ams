e4e859e9acdcef344d077d8d1e4967dfd259a027
hp <469331022@qq.com>
2018-08-05 17:42:13 +0800

    提交1

--- application/index/controller/Collect.php
+++ application/index/controller/Collect.php
@@ -4,12 +4,13 @@ use app\index\model\Collect as collectModel ;
 use think\Controller;
 class Collect extends Controller
 {
+    protected $collectModel;
     /*
      * 构造函数
      *
      */
-    public function initialize(){
-        $collectModel=new collectModel();
+    public function _initialize(){
+        $this->collectModel=new collectModel();
     }
 	/*
      * 收到礼金检索
@@ -66,7 +67,7 @@ class Collect extends Controller
                     $new_data['remark']=$data['remark'];
                 }
                 //调用model层add方法插入数据
-                $res=$collectModel ->add($new_data);
+                $res=$this->collectModel->add($new_data);
                 return json_encode($res);
 
             }
